ee5dd32244594ced65d8a6bf7bcd5ed2fab216b9
Candy <Jazmincandy2224@gmail.com>
2020-11-15 20:21:07 -0600

    Examen final

---
+++ Agente 1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+using Experimental.System.Messaging;
+
+namespace Agente_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            Console.WriteLine("----------------------------------------------------------------------\n");
+            Console.WriteLine("                              Agente 1                                 \n");
+        
+            Console.WriteLine("Deseas enviar nombres aleatorios a la cola?\n S. Si \n N. No");
+            string ne=Console.ReadLine();
+
+            while (ne != "N")
+            {
+               //Mensaje
+             string result = Path.GetRandomFileName();
+        
+
+             //Creación de la conexión con la cola 
+             MessageQueue servidor = new MessageQueue(".\\Private$\\Nombres");
+             //int iCount = Queue.GetAllMessages().count();
+
+             //Convertir el mensaje
+             String jsonMessage=JsonConvert.SerializeObject(result);
+
+             //Empaquetar el mensaje
+             //Message msg = new Message();
+             // msg.Body=jsonMessage;
+
+             //Enviar mensaje
+             Console.WriteLine("Nombre aleatorio enviado");
+             Console.WriteLine(result);
+             servidor.Send(result);
+        
+            }
+        
+        }
+
+    }
+    public class Messagenom
+    {
+       public string Nombre {get; set;}
+    }
+
+
+}
+
---
+++ Agente2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+using Google.Cloud.Firestore;
+using Experimental.System.Messaging;
+
+namespace Agente2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("----------------------------------------------------------------------\n");
+            Console.WriteLine("                              Agente 2                                 \n");
+
+            Console.WriteLine("Deseas imprimir la cola?\n S. Si \n N. No");
+            string r=Console.ReadLine();
+        
+
+            while (r != "N")
+            {
+     
+             Console.WriteLine("Imprimiendo Cola.......\n");
+             //Creación de la conexión con la cola 
+             MessageQueue servidor = new MessageQueue(".\\Private$\\Nombres");
+
+             //pop
+             Message messajeJson=servidor.Receive();
+             messajeJson.Formatter= new XmlMessageFormatter(new String[] { "System.String,mscorlib" });
+             Console.WriteLine(messajeJson.Body);
+            
+          
+             //Conexión al archivo de texto
+             TextWriter escribir = new StreamWriter(@"C:\Users\Jazmi\Downloads\Cola.txt");
+             escribir.WriteLine(messajeJson.Body);
+             escribir.Close();
+             
+            }
+
+        }
+    }
+    public class Messagenom
+    {
+       public string Nombre {get; set;}
+    }
+
+}
